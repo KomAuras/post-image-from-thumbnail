0144167a10f6aa66b62487f63a15e0328c6fabd6
Evgeny Stefanenko <mixer@clarionlife.net>
2016-12-14 17:56:20 +1000

    Fix first publish

--- pift.php
+++ pift.php
@@ -7,17 +7,19 @@ Author: Evgeny Stefanenko
 Author URI: www.millenniumfoto.com
 */
 
-add_filter( 'wp_insert_post_data' , 'filter_post_data' , '99', 2 );
+add_action( 'save_post' , 'pift_save_post' , '99', 3 );
 
-function filter_post_data( $data , $postarr ) {
-    if( ! isset($_POST['post_type']) || $_POST['post_type'] != 'post' ) return $data; // убедимся что мы редактируем нужный тип поста
-    if( get_current_screen()->id != 'post' ) return $data; // убедимся что мы на нужной странице админки
-    if ( defined('DOING_AUTOSAVE') && DOING_AUTOSAVE  ) return $data; // пропустим если это автосохранение
-    if ( ! current_user_can('edit_post', $postarr['ID'] ) ) return $data; // убедимся что пользователь может редактировать запись
-
-    $attach_id = get_post_thumbnail_id($postarr->ID);
-    if ($attach_id && !preg_match('/cthulhu/', $data['post_content'])) {
-        $data['post_content'] = wp_get_attachment_image($attach_id, 'medium', false, array('class'=>'cthulhu attachment-medium alignright')).$data['post_content'];
+function pift_save_post( $post_ID, $post , $update ) {
+    if ( $post->post_type != 'post' || wp_is_post_revision( $post_id ) || $post->post_status != 'publish' )
+        return;
+    $attach_id = get_post_thumbnail_id($post_ID);
+    if ($attach_id && !preg_match('/cthulhu/', $post->post_content)) {
+        $my_post = array(
+            'ID'            => $post_ID,
+            'post_content'  => wp_get_attachment_image($attach_id, 'medium', false, array('class'=>'cthulhu attachment-medium alignright')).$post->post_content
+        );
+        remove_action( 'save_post', 'pift_save_post' );
+        $post_id = wp_update_post( $my_post );
+        add_action( 'save_post' , 'pift_save_post' , '99', 3 );
     }
-    return $data;
 }
